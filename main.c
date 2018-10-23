e2de8cf4218acba18fdb74b4cb0b6e42f736c683
johnny <jadonoso2@miuandes.cl>
2018-10-23 12:12:09 -0300

    de a poco

--- main.c
+++ main.c
@@ -15,10 +15,109 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
-void page_fault_handler( struct page_table *pt, int page )
+int *tabla_marcos;
+int npages,nframes;
+struct disk *disk;
+char *physmem;
+int nframes;
+const char *metodo;
+
+struct index{
+	int frame;
+	int page;
+	struct index *sgte;};
+
+struct lista{
+	struct index *cabeza;};
+
+struct lista *table_index;
+
+void add_index(int frame, int page, struct index *cabeza,struct index *next){
+	next = cabeza;
+	if(next!=NULL){
+		while(next->sgte != NULL){
+			next = next->sgte;}
+		}
+	next->sgte = malloc(sizeof(struct index));
+	next = next->sgte;
+	next->frame = frame;
+	next->page = page;
+	}
+
+
+void page_fault_handler_propio( struct page_table *pt, int page )
 {
 	printf("page fault on page #%d\n",page);
-	exit(1);
+	//exit(1);
+}
+void page_fault_handler_fifo( struct page_table *pt, int page )
+{
+	printf("page fault on page #%d\n",page);
+	//exit(1);
+}
+void page_fault_handler_random( struct page_table *pt, int page )
+{
+	int nframe = page_table_get_nframes(pt);
+	int rand_frame = lrand48() % nframe;
+	int page2;
+	fprintf(stderr,"entro");
+	struct index *aux = table_index->cabeza;//no retorna nada, arreglar, falta inicializar table_index antes de esto.
+	
+	while(aux->sgte!= NULL){
+		if(aux->frame==rand_frame){
+			page2 = aux->page;
+			
+			break;}
+		aux = aux->sgte;}
+	disk_write(disk,page2,&physmem[rand_frame * PAGE_SIZE]);
+	disk_read(disk,page,&physmem[rand_frame *PAGE_SIZE]);
+	
+	struct index *aux2 = table_index->cabeza;
+	while(aux2->sgte!= NULL){
+		if(aux2->frame==rand_frame){
+			aux2->page = page;;
+			break;}
+		aux2 = aux2->sgte;}
+	page_table_set_entry(pt,page2,rand_frame,0);
+	page_table_set_entry(pt,page,rand_frame, PROT_READ|PROT_WRITE|PROT_EXEC);
+}
+
+void page_fault_handler(struct page_table *pt, int page){
+	//creando tabla de marcos-->
+	int auxmetodo = 1;
+	for(int i = 0; i<nframes;i++){
+		if(tabla_marcos[i]!=0){//marco ocupado, 1 marco desocupado;
+			disk_read(disk,page,&physmem[i * PAGE_SIZE]);
+			page_table_set_entry(pt,page,i, PROT_READ|PROT_WRITE|PROT_EXEC);
+			tabla_marcos[i]= 0;
+			struct index *aux1 = table_index->cabeza;
+			if(i==0){
+				table_index->cabeza->frame = 0;
+				table_index->cabeza->page = page;
+				auxmetodo=0;
+				break;
+				}
+			add_index(i,page,table_index->cabeza,aux1);
+			auxmetodo = 0;
+			break;
+		}
+	}
+	if (auxmetodo == 1){
+		int aux2=1;
+		if(strcmp(metodo,"random")==0){
+			printf("entro");
+			page_fault_handler_random(pt,page);
+			aux2=2;}
+		if(strcmp(metodo,"fifo")==0){
+			page_fault_handler_fifo(pt,page);
+			aux2=2;}
+		if(strcmp(metodo,"propio")==0){
+			page_fault_handler_propio(pt,page);
+			aux2=2;}
+		if(aux2==1){
+		printf("método %s desconocido",metodo);
+		exit(1);}
+	}
 }
 
 int main( int argc, char *argv[] )
@@ -31,6 +130,7 @@ int main( int argc, char *argv[] )
 
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
+	metodo = argv[3];
 	const char *program = argv[4];
 
 	struct disk *disk = disk_open("myvirtualdisk",npages);
@@ -38,18 +138,28 @@ int main( int argc, char *argv[] )
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
 		return 1;
 	}
+	
+	printf("entro 3");
+	int m[nframes];
+	tabla_marcos = m;
 
+	for(int d=0;d<nframes;d++){
+		tabla_marcos[d]=1;}
 
-	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
-	if(!pt) {
-		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
-		return 1;
-	}
+	printf("entro2");
+	struct page_table *pt;
+	
+	pt = page_table_create(npages,nframes,page_fault_handler);
 
 	char *virtmem = page_table_get_virtmem(pt);
 
-	char *physmem = page_table_get_physmem(pt);
+	physmem = page_table_get_physmem(pt);
+	
+	for(int j =0; j<nframes;j++){
+		disk_write(disk,j,&physmem[j* BLOCK_SIZE]);}
+	printf("entro1");
 
+	
 	if(!strcmp(program,"sort")) {
 		sort_program(virtmem,npages*PAGE_SIZE);
 
@@ -63,6 +173,8 @@ int main( int argc, char *argv[] )
 		fprintf(stderr,"unknown program: %s\n",argv[3]);
 
 	}
+	
+	page_table_print(pt);
 
 	page_table_delete(pt);
 	disk_close(disk);
